2192c24ac58d5b7af65eb6f45a16f7f3819c9035
4shimi <king81597@snu.ac.kr>
2022-08-23 15:16:08 +0900

    server requesting
    server requesting

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,55 @@
 import ReviewList from "./ReviewList";
-import items from "../mock.json";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { getReviews } from "../api";
+
+const LIMIT = 6;
 
 function App() {
+  const [items, setItems] = useState([]);
   const [order, setOrder] = useState("createdAt");
+  const [offset, setOffset] = useState(0);
+  const [hasNext, setHasNext] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
   const handleNewestClick = () => setOrder("createdAt");
   const handleBestClick = () => setOrder("rating");
+
+  const handleDelete = (id) => {
+    const nextItems = items.filter((item) => item.id !== id);
+    setItems(nextItems);
+  };
+
+  const handleLoad = async (options) => {
+    let result;
+    try {
+      setIsLoading(true);
+      result = await getReviews(options);
+    } catch (error) {
+      console.error(error);
+      return;
+    } finally {
+      setIsLoading(false);
+    }
+
+    const { paging, reviews } = result;
+    if (options.offset === 0) {
+      setItems(reviews);
+    } else {
+      setItems((preItems) => [...preItems, ...reviews]);
+    }
+    setOffset(options.offset + reviews.length);
+    setHasNext(paging.hasNext);
+  };
+
+  const handleLoadMore = () => {
+    handleLoad({ order, offset, limit: LIMIT });
+  };
+
+  useEffect(() => {
+    handleLoad({ order, offset: 0, limit: LIMIT });
+  }, [order]);
 
   return (
     <div>
@@ -15,7 +57,12 @@ function App() {
         <button onClick={handleNewestClick}>최신순</button>
         <button onClick={handleBestClick}>평점순</button>
       </div>
-      <ReviewList items={sortedItems} />
+      <ReviewList items={sortedItems} onDelete={handleDelete} />
+      {hasNext && (
+        <button disabled={isLoading} onClick={handleLoadMore}>
+          더보기
+        </button>
+      )}
     </div>
   );
 }
